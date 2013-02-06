9075d931b7921993cdd1f037589dd59b8c7fc717
japacible <japacible@gmail.com>
2013-02-06 12:50:06 -0800

    gmail spammer

---
+++ mail.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*- 
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "MY_HANDLE@GMAIL.COM"
+gmail_pwd = "MY_PASSWORD"
+
+def mail(to, subject, text):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   mailServer.close()
+
+for x in range(1, 601):
+    mail("HANDLE@DOMAIN.com",
+       "SUBJECT: %d" % (x),
+       u"(´･ω･`)")
